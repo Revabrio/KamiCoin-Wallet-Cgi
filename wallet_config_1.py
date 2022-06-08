f28a76b77d667c359a0c6dd1bd08df72a115ddbf
Revabrio <49591194+Revabrio@users.noreply.github.com>
2022-06-09 00:37:45 +0300

    Delete wallet_config_1.py

--- wallet_config_1.py
+++
@@ -1,2 +0,0 @@
-public_key = '9UKrr+qZbB4dzSACNbJL3bhboy1lVA77Nvy/7QaFB5ZU2B67rOvYEdm7k4pZjAaII2XTByKoHZtB0+g5K2OmMw=='
-private_key = '21ee321e26914a9837382eea5be600c5ace99d1bde30b23ed6bb3f35ddb5c46f'
